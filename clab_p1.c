c6f89209ccae8c9136732400df717e38ed9dba4a
Joel Robertson <jdrobertson@cyberland.co.nz>
2016-05-03 22:39:18 +1200

    First two methods defined
    Compiles

--- clab_p1.c
+++ clab_p1.c
@@ -1,9 +1,13 @@
 #include "stdio.h"
 
+#define PRECEDENCE "+-*/%%^"
+
 struct stackNode {
 	char data;
 	struct stackNode *nextPtr;
 };
+
+
 
 typedef struct stackNode StackNode;
 
@@ -35,9 +39,26 @@ int main(int argc, char const *argv[])
 
 void convertToPostfix( char infix[], char postfix[] );
 
-int isOperator( char c ){}
+int isOperator( char c ){
+	int i;
+	for(i = 0; i<sizeof(PRECEDENCE); i++){
+		if(PRECEDENCE[i]==c) return 1;
+	}
+	return 0;
+}
 
-int precendence ( char operator1, char operator2 ){}
+int precendence ( char operator1, char operator2 ){
+	int i, j;
+	for(i = 0; i<sizeof(PRECEDENCE); i++){
+		if(PRECEDENCE[i]==operator1) break;
+	}
+	for(j = 0; j<sizeof(PRECEDENCE); j++){
+		if(PRECEDENCE[j]==operator2) break;
+	}
+	if(i<j) return 1;
+	if(i==j) return 0;
+	return -1;
+}
 
 void push( StackNodePtr *topPtr, char value){}
 
